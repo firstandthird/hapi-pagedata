51887891aaa942c1154fb54b564414acb320c482
Greg Allen <greg@firstandthird.com>
2016-12-02 14:45:38 -0800

    fixed validation for userAgent

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ exports.register = function(server, options, next) {
     cache: Joi.object().allow(null),
     cacheEndpoint: Joi.string().allow(false),
     hookEndpoint: Joi.string().allow(false),
+    userAgent: Joi.string().allow(''),
     verbose: Joi.boolean()
   });
 
